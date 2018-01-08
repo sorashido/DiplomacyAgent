3a0667326c11002de69d1d8098a780d36c83640f
shido <poporotylm@gmail.com>
2018-01-08 23:52:01 +0900

    commit

--- src/sorashido/DDAgent3/DDAgent.scala
+++ src/sorashido/DDAgent3/DDAgent.scala
@@ -2,15 +2,18 @@ package sorashido.DDAgent3
 
 import ddejonge.bandana.anac.ANACNegotiator
 import ddejonge.bandana.dbraneTactics.DBraneTactics
+import ddejonge.bandana.dbraneTactics.Plan
 import ddejonge.bandana.negoProtocol._
 import ddejonge.bandana.tools.Utilities
-import es.csic.iiia.fabregues.dip.orders.{HLDOrder, MTOOrder, Order}
+import ddejonge.negoServer.Message
+import es.csic.iiia.fabregues.dip.orders._
 import es.csic.iiia.fabregues.dip.board.{Power, Province, Region}
 import java.util.Random
 import java.util.ArrayList
 
 import scala.collection.JavaConversions._
 import sorashido.DDAgent3.util.{Constants, Predictable}
+import sorashido.DDAgent3.negotiation.ProposeDeal
 
 object DDAgent3 {
   def main(args: Array[String]): Unit = {
@@ -20,12 +23,13 @@ object DDAgent3 {
 }
 
 class DDAgent3 private(val args: Array[String]) extends ANACNegotiator(args) {
-  dBraneTactics = this.getTacticalModule
-  final private[DDAgent3] val printToConsole = false
+//  var dBraneTactics = this.getTacticalModule
+  final private[DDAgent3] val printToConsole = true
 //  final private[DDAgent3] val constants = new Constants
   final private[DDAgent3] val constants = Constants
   private[DDAgent3] var dBraneTactics = new DBraneTactics
   private val random = new Random
+  val proposedeal = new ProposeDeal()
 
   override def start(): Unit = {
     this.getLogger.logln("game is starting!", printToConsole)
@@ -34,8 +38,8 @@ class DDAgent3 private(val args: Array[String]) extends ANACNegotiator(args) {
   override def negotiate(negotiationDeadline: Long): Unit = {
     while (System.currentTimeMillis < negotiationDeadline) {
       while (hasMessage) manageProposedMessage()
-
-      proposeMessage()
+//
+        proposeMessage()
 
       val predict = Predictable
     }
@@ -78,7 +82,7 @@ class DDAgent3 private(val args: Array[String]) extends ANACNegotiator(args) {
 
         if (consistencyReport == "") {
           val opponent = game.getPower(receivedMessage.getSender())
-          if (calcPlanValue(commitments, opponent) > 0.5) {
+          if (calcPlanValue(commitments) > 0.5) {
             this.acceptProposal(receivedProposal.getId)
             this.getLogger.logln("DDAgent3.negotiate()  Accepting: " + receivedProposal, printToConsole)
           }
@@ -99,113 +103,11 @@ class DDAgent3 private(val args: Array[String]) extends ANACNegotiator(args) {
   }
 
   private def proposeMessage(): Unit = {
-    val newDealToPropose = searchForNewDealToPropose
-    if (!newDealToPropose.getDemilitarizedZones.isEmpty || !newDealToPropose.getOrderCommitments.isEmpty) {
-      this.getLogger.logln("DDAgent3.negotiate() Proposing: " + newDealToPropose, printToConsole)
-      this.proposeDeal(newDealToPropose)
-    }
+    val newDealToPropose = proposedeal.searchForNewDealToPropose(game, me, dBraneTactics, this.getConfirmedDeals, this.getNegotiatingPowers)
   }
 
-  def searchForNewDealToPropose: BasicDeal = {
-    val commitments = new ArrayList[BasicDeal]
-    var bestValue = 1.0
-    commitments.addAll(this.getConfirmedDeals)
-    var bestDeal = generateRandomDeal
-    for(i <- 0 to 9) {
-      val randomDeal = generateRandomDeal
-      if (randomDeal != null) {
-        commitments.add(randomDeal)
-        val value = calcPlanValue(commitments)
-        if (value > bestValue) {
-          bestDeal = randomDeal
-          bestValue = value
-        }
-        commitments.remove(commitments.size - 1)
-      }
-    }
-    bestDeal
-  }
-
-  def generateRandomDeal: BasicDeal = { //Get the names of all the powers that are connected to the negotiation server and which have not been eliminated.
-    val aliveNegotiatingPowers = this.getNegotiatingPowers
-    val numAliveNegoPowers = aliveNegotiatingPowers.size
-    if (numAliveNegoPowers < 2) return null
-    val demilitarizedZones = new ArrayList[DMZ](3)
-
-    for(i <- 0 to 2) {
-      val powers = new ArrayList[Power](2)
-      powers.add(me)
-      var randomPower = me
-      while ( {
-        randomPower.equals(me)
-      }) {
-        val numNegoPowers = aliveNegotiatingPowers.size
-        randomPower = aliveNegotiatingPowers.get(random.nextInt(numNegoPowers))
-      }
-      powers.add(randomPower)
-
-      val provinces = new ArrayList[Province]
-      for(j <- 0 to 2) {
-        val numProvinces = this.game.getProvinces.size
-        val randomProvince = this.game.getProvinces.get(random.nextInt(numProvinces))
-        provinces.add(randomProvince)
-      }
-      demilitarizedZones.add(new DMZ(game.getYear, game.getPhase, powers, provinces))
-    }
-
-    val randomOrderCommitments = new ArrayList[OrderCommitment]
-    val units = new ArrayList[Region]
-    for (power <- aliveNegotiatingPowers) {
-      units.addAll(power.getControlledRegions)
-    }
-
-    for(i <- 0 to 2) {
-      if (units.size != 0) {
-        val randomUnit = units.remove(random.nextInt(units.size))
-        val power = game.getController(randomUnit)
-        val potentialDestinations = new ArrayList[Region]
-        val adjacentRegions = randomUnit.getAdjacentRegions
-        adjacentRegions.add(randomUnit)
-        for (adjacentRegion <- adjacentRegions) {
-          val adjacentProvince = adjacentRegion.getProvince
-          //Check that the adjacent Region is not demilitarized for the power controlling the unit.
-          var isDemilitarized = false
-          for (dmz <- demilitarizedZones) {
-            if (dmz.getPowers.contains(power) && dmz.getProvinces.contains(adjacentProvince)) {
-              isDemilitarized = true
-            }
-          }
-          if (!isDemilitarized) potentialDestinations.add(adjacentRegion)
-        }
-        val numPotentialDestinations = potentialDestinations.size
-        if (numPotentialDestinations > 0) {
-          val randomDestination = potentialDestinations.get(random.nextInt(numPotentialDestinations))
-          if (randomDestination.equals(randomUnit)) {
-            val randomOrder = new HLDOrder(power, randomUnit)
-            randomOrderCommitments.add(new OrderCommitment(game.getYear, game.getPhase, randomOrder))
-          }
-          else {
-            val randomOrder = new MTOOrder(power, randomUnit, randomDestination)
-            randomOrderCommitments.add(new OrderCommitment(game.getYear, game.getPhase, randomOrder))
-          }
-        }
-      }
-    }
-    val deal = new BasicDeal(randomOrderCommitments, demilitarizedZones)
-    deal
-  }
-
-  private def calcPlanValue(commitments: ArrayList[BasicDeal], opponent: Power): Double = {
-    val myPlan = this.dBraneTactics.determineBestPlan(game, me, commitments)
-    val opPlan = this.dBraneTactics.determineBestPlan(game, opponent, commitments)
-    if (myPlan == null || opPlan == null) { //取り決めのために行動できない -> 交渉する必要なし
-      return 0.0
-    }
-    myPlan.getValue
-  }
-
-  private def calcPlanValue(commitments: ArrayList[BasicDeal]): Double = {
-    val myPlan = this.dBraneTactics.determineBestPlan(game, me, commitments)
+  def calcPlanValue(commitments: ArrayList[BasicDeal]): Double = {
+    val myPlan = this.dBraneTactics.determineBestPlan(this.game, me, commitments)
     if (myPlan == null) { //取り決めのために行動できない -> 交渉する必要なし
       return 0.0
     }
